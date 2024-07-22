ca5e829a4bf4bba5247e2b8c0d49ac51e8c15168
HafiidhFbriant03 <157139402+HafiidhFbriant03@users.noreply.github.com>
2024-07-22 14:37:50 +0700

    hapus router

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AdminController;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
@@ -14,6 +14,4 @@ Route::post('/auth', [AuthController::class, 'post_login']);
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/categori', [CategoryController::class, 'index']);
 
-
 //router admin
-Route::get('/admin', [AdminController::class, 'index']);
